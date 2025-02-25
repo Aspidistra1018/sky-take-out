e8d6f81eed58a66697e17c261bdbf9f628ba1525
Aspidistra <Aspidistra@1018.com>
2025-02-25 20:16:33 +0800

    导入地址簿相关功能代码

---
+++ Back/sky-take-out/sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -0,0 +1,41 @@
+package com.sky.mapper;
+
+import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Mapper
+public interface ShoppingCartMapper {
+
+    List<ShoppingCart> list(ShoppingCart shoppingCart);
+
+    /**
+     * 根据id查询商品数量
+     *
+     * @param shoppingCart
+     */
+    @Update("update shopping_cart set number = #{number} where id = #{id}")
+    void updateNumberById(ShoppingCart shoppingCart);
+
+    /**
+     * 插入购物车数据
+     * @param shoppingCart
+     */
+    @Insert("insert into shopping_cart (name, image, user_id, dish_id, setmeal_id, dish_flavor, number, amount, create_time) VALUES " +
+            "(#{name},#{image},#{userId},#{dishId},#{setmealId},#{dishFlavor},#{number},#{amount},#{createTime})")
+    void insert(ShoppingCart shoppingCart);
+
+    @Delete("delete from shopping_cart where user_id = #{userId}")
+    void deleteByUserId(Long userId);
+
+    int getCount(ShoppingCart shoppingCart);
+
+
+    void subShoppingCart(ShoppingCart shoppingCart);
+
+    @Delete("delete from shopping_cart where id = #{id}")
+    void deleteById(Long id);
+
+}
