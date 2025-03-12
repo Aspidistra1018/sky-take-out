7525979b32fa4b967fe67fbb43eb4ecd284e76d7
Aspidistra <Aspidistra@1018.com>
2025-03-12 19:15:40 +0800

    图标显示代码开发

--- Back/sky-take-out/sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ Back/sky-take-out/sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -1,8 +1,11 @@
 package com.sky.mapper;
 
 import com.sky.entity.User;
+import io.swagger.models.auth.In;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.Map;
 
 @Mapper
 public interface UserMapper {
@@ -26,4 +29,18 @@ public interface UserMapper {
      */
     @Select("select * from user where id= #{userId}")
     User getById(Long userId);
+
+    /**
+     * 根据日期查找总用户
+     * @param map
+     * @return
+     */
+    Integer totalUserSumByMap(Map map);
+
+    /**
+     * 根据日期查找新用户
+     * @param map
+     * @return
+     */
+    Integer newUserSumByMap(Map map);
 }
--- Back/sky-take-out/sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ Back/sky-take-out/sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -2,9 +2,12 @@ package com.sky.service.impl;
 
 import com.aliyuncs.ecs.model.v20140526.DetachClassicLinkVpcRequest;
 import com.sky.entity.Orders;
+import com.sky.entity.User;
 import com.sky.mapper.OrderMapper;
+import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.util.StringUtil;
@@ -26,6 +29,9 @@ public class ReportServiceImpl implements ReportService {
 
     @Autowired
     private OrderMapper orderMapper;
+
+    @Autowired
+    private UserMapper userMapper;
 
     /**
      * 统计指定时间区间内的营业额数据
@@ -62,6 +68,56 @@ public class ReportServiceImpl implements ReportService {
         return TurnoverReportVO.builder()
                 .dateList(StringUtils.join(dateList, ","))
                 .turnoverList(StringUtils.join(turnoverList, ","))
+                .build();
+    }
+
+    /**
+     * 统计指定时间区间内的用户数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    public UserReportVO getUserStatistics(LocalDate begin, LocalDate end) {
+        //当前集合用于存放从begin到end范围内的每天的日期
+        List<LocalDate> dateList = new ArrayList<>();
+        dateList.add(begin);
+        while (!begin.equals(end)) {
+            //日期计算，计算指定日期的最后一天对应的日期
+            begin = begin.plusDays(1);
+            dateList.add(begin);
+        }
+        //将每一天的总人数放入totalUserList集合内
+        List<Integer> totalUserList = new ArrayList<>();
+        for (LocalDate date : dateList){
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);
+
+            Map map = new HashMap();
+            map.put("end", endTime);
+            Integer totalUser =  userMapper.totalUserSumByMap(map);
+            totalUserList.add(totalUser);
+
+        }
+        //将每一天的新用户人数放入totalUserList集合内
+        List<Integer> newUserList = new ArrayList<>();
+        for (LocalDate date : dateList){
+            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);
+
+            Map map = new HashMap();
+            map.put("begin", beginTime);
+            map.put("end", endTime);
+            Integer newUser =  userMapper.newUserSumByMap(map);
+            newUserList.add(newUser);
+
+        }
+
+
+
+
+        return UserReportVO.builder()
+                .dateList(StringUtils.join(dateList, ","))
+                .totalUserList(StringUtils.join(totalUserList, ","))
+                .newUserList(StringUtils.join(newUserList, ","))
                 .build();
     }
 }
